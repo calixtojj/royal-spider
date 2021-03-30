7d937970a648cb4ff798dd53fb1ab74056b36c29
Stackbit Code Editor <projects@stackbit.com>
2021-03-30 15:56:46 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -16,10 +16,10 @@ export default class HeroSection extends React.Component {
                 <div className="hero__title">
                     <script type="text/javascript">
                         var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;,
-  this.el = el;,
-  this.loopNum = 0;,
-  this.period = parseInt(period, 10) || 2000;,
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
